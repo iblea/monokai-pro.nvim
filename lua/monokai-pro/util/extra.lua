6d29fb4a16d33c7db60e52ac54c44561663850f2
iblea <iblea@iasdf.com>
2025-08-17 11:46:44 +0900

    fix: terminal blue ANSI color print

--- lua/monokai-pro/util/extra.lua
+++ lua/monokai-pro/util/extra.lua
@@ -21,7 +21,7 @@ function M.terminal(colors)
   vim.g.terminal_color_3 = colors.base.yellow
   vim.g.terminal_color_11 = colors.base.yellow
 
-  vim.g.terminal_color_4 = colors.base.blue
+  -- vim.g.terminal_color_4 = colors.base.blue
   vim.g.terminal_color_12 = colors.base.blue
 
   vim.g.terminal_color_5 = colors.base.magenta
